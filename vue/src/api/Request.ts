7502adf97bf1e58289f7e88db656c22be7b80f20
digyth <wzm8011413@126.com>
2022-06-03 10:26:50 +0800

    fix json bug

--- vue/src/api/Request.ts
+++ vue/src/api/Request.ts
@@ -4,7 +4,11 @@ class Request{
     public inst=axios.create({
         baseURL: "http://localhost:8081/api/",
         timeout: 1000,
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
         withCredentials: true
     });
 }
